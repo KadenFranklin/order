f92e42fe7936e6d7967fbaa5175c9ec3b001416d
KadenFranklin <43596236+KadenFranklin@users.noreply.github.com>
2021-02-15 09:21:06 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,11 @@ fn main() -> io::Result<()> {
         let args = env::args()
             .skip(2)
             .collect::<Vec<String>>();
+        let mut line_list = vec![];
         for file in args.clone() {
-            let mut line_list = vec![];
             let file_contents = fs::read_to_string(&file)?;
             for lines in file_contents.lines() {
-                &line_list.push(lines);
+                &line_list.push(lines.to_string());
                 &line_list.sort();
                 &line_list.reverse();
                 if lines == file_contents.lines().last().unwrap() {
@@ -26,11 +26,11 @@ fn main() -> io::Result<()> {
         let args = env::args()
             .skip(1)
             .collect::<Vec<String>>();
+        let mut line_list = vec![];
         for file in args.clone() {
-            let mut line_list = vec![];
             let file_contents = fs::read_to_string(&file)?;
             for lines in file_contents.lines() {
-                &line_list.push(lines);
+                &line_list.push(lines.to_string());
                 &line_list.sort();
                 if lines == file_contents.lines().last().unwrap(){
                     for line in line_list.clone() {
