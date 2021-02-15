a14f99bc8e6e0371c855f2deba266e83736a3715
KadenFranklin <43596236+KadenFranklin@users.noreply.github.com>
2021-02-15 00:25:52 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,50 +3,42 @@ use std::{env, io, fs};
 fn main() -> io::Result<()> {
     let args = env::args().collect::<Vec<String>>();
     let var = &args[1];
-
     if var == "-r" {
-        let args = env::args().skip(1);
-        for file in args.skip(1) {
-            println!("{}",file);
+        let args = env::args()
+            .skip(2)
+            .collect::<Vec<String>>();
+        for file in args.clone() {
+            let mut line_list = vec![];
             let file_contents = fs::read_to_string(&file)?;
-            let line_list = sort_1(file_contents);
-            for line in line_list {
-                println!("{}", line);
+            for lines in file_contents.lines() {
+                &line_list.push(lines);
+                &line_list.sort();
+                &line_list.reverse();
+                if lines == file_contents.lines().last().unwrap() {
+                    for line in line_list.clone() {
+                        println!("{}", line);
+                    }
+                }
             }
         }
     }
     else {
-        let args = env::args();
-        for file in args {
-            println!("{}",file);
+        let args = env::args()
+            .skip(1)
+            .collect::<Vec<String>>();
+        for file in args.clone() {
+            let mut line_list = vec![];
             let file_contents = fs::read_to_string(&file)?;
-            let line_list = sort_2(file_contents);
-            for line in line_list {
-                println!("{}", line);
+            for lines in file_contents.lines() {
+                &line_list.push(lines);
+                &line_list.sort();
+                if lines == file_contents.lines().last().unwrap(){
+                    for line in line_list.clone() {
+                        println!("{}", line);
+                    }
+                }
             }
         }
     }
     Ok(())
-}
-
-
-fn sort_1(file_contents : String) -> Vec<&'static str> {
-
-    let mut line_list= vec![];
-    for lines in file_contents.lines() {
-        line_list.push(lines);
-        line_list.sort();
-    }
-    return line_list
-}
-
-fn sort_2(file_contents : String) -> Vec<&'static str> {
-
-    let mut line_list= vec![];
-    for lines in file_contents.lines() {
-        line_list.push(lines);
-        line_list.sort();
-        line_list.reverse()
-    }
-    return line_list
 }
